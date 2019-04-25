6022db4bbc971487ae312cdb082b90e3bc764d6f
RyanVillenaUCR <rvill028@ucr.edu>
2019-04-25 16:21:49 -0700

    Update build instructions to reflect new paths

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,5 +1,5 @@
-bison -v -d --file-prefix=y calc.y \
-&& flex mini_l.lex \
-&& gcc -o calc y.tab.c lex.yy.c -lfl \
-&& ./calc $1
+bison -v -d --file-prefix=y phase2/calc.y \
+&& flex phase2/mini_l.lex \
+&& gcc -o phase2/calc phase2/y.tab.c phase2/lex.yy.c -lfl \
+&& ./phase2/calc $1
 
