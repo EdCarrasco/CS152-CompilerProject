ebef23fced412022d7e09015dd872845f0c4a321
RyanVillenaUCR <rvill028@ucr.edu>
2019-04-25 16:37:40 -0700

    Use build files from phase2 folder

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,5 +1,7 @@
 bison -v -d --file-prefix=y phase2/calc.y \
 && flex phase2/mini_l.lex \
+&& mv -f y.* phase2/ \
+&& mv -f lex.yy.c phase2/ \
 && gcc -o phase2/calc phase2/y.tab.c phase2/lex.yy.c -lfl \
 && ./phase2/calc $1
 
